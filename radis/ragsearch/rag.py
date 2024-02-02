0a4e8998456f0e75b259f1403348db86ea6b97e9
Hagen <13981097+hagisgit@users.noreply.github.com>
2024-02-02 03:27:18 +0100

    add openai interface

--- radis/ragsearch/rag.py
+++ radis/ragsearch/rag.py
@@ -1,10 +1,16 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(base_url=os.environ["BACKEND_URL"], api_key=os.environ["OPENAI_API_KEY"])
+
 SYSTEM_PROMT = """Your are a helpful assistant with medical knowledge in the field of raiology. 
 Your task is to help users to sort out relevant entries in a list of radiological reports, given 
 a specific question about the contents of each of the reports. In the following, the list of reports 
 is presented to you between the markers [BEGINLIST] and [ENDLIST]. Each report is enclosed between 
 the markers [BEGINREPORT] and [ENDREPORT]. Each report has an ID, which is in the first line of the report, 
 followed by a newline and the report text. The question is given between the markers [BEGINQUESTION] and [ENDQUESTION].
-In your answer, please only list the ID of the reports that are relevant to the question in python integer array notation.
+Do not answer verbally. Only list the ID of the reports that are relevant to the question in python integer array notation without any additional text.
+Here is the list:
 """
 
 
@@ -17,9 +23,35 @@ class Rag(object):
         self.run()
 
     def run(self):
+        report_body_list = []
         for report in self.reports:
             report_full = report.report_full
+            report_body_list.append(report_full.body)
             # print(report_full.id, "\n", report_full.body, "\n\n")
+        n = 0
+        c = 0
+        reports_string = ""
+        while n < len(report_body_list):
+            if c < 5:
+                reports_string += f"[BEGINREPORT] {n}\n{report_body_list[n]} [ENDREPORT]\n"
+                n += 1
+                c += 1
+            else:
+                prompt = (
+                    f"<s>[INST] {SYSTEM_PROMT} \n[BEGINLIST]\n{reports_string}\n[ENDLIST]  [/INST]"
+                )
+                response = client.completions.create(
+                    model=os.environ["LLM_MODEL"],
+                    prompt=prompt,
+                    max_tokens=2048,
+                    stream=False,
+                )
+                print((response.choices[0].text))
+                c = 0
+                reports_string = ""
+                reports_string += f"[BEGINREPORT] {n}\n{report_body_list[n]} [ENDREPORT]\n"
+                n += 1
+                c += 1
         self._ready = True
 
     def ready(self):
