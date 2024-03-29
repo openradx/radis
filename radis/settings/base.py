9f19446f623d3035de6156b50ac4af2210608777
Kai Schlamp <kai.schlamp@gmail.com>
2024-03-29 14:43:47 +0000

    Set fixed site parameters in settings file

--- radis/settings/base.py
+++ radis/settings/base.py
@@ -44,16 +44,10 @@ SITE_ID = 1
 # our custom SiteProfile model.
 # Once set those values will be used from the database!
 SITE_DOMAIN = env.str("SITE_DOMAIN", default="radis.test")  # type: ignore
-SITE_NAME = env.str("SITE_NAME", default="RADIS")  # type: ignore
-SITE_META_KEYWORDS = env.str(
-    "SITE_META_KEYWORDS",
-    default="RADIS,Radiology,Reports,Medicine,Tool",  # type: ignore
-)
-SITE_META_DESCRIPTION = env.str(
-    "SITE_META_DESCRIPTION",
-    default="RADIS is an application to archive, query and collect radiology reports",  # type: ignore
-)
-SITE_PROJECT_URL = env.str("SITE_PROJECT_URL", default="https://github.com/openradx/radis")  # type: ignore
+SITE_NAME = "RADIS"
+SITE_META_KEYWORDS = "RADIS,Radiology,Reports,Medicine,Tool"
+SITE_META_DESCRIPTION = "RADIS is an application to archive, query and collect radiology reports"
+SITE_PROJECT_URL = "https://github.com/openradx/radis"
 
 INSTALLED_APPS = [
     "daphne",
