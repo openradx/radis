a03e8d422d6ec81578f6d9a48e3d63aebfba06fd
Kai Schlamp <kai.schlamp@gmail.com>
2024-05-20 14:30:47 +0000

    Add query parser

---
+++ radis/search/tests/test_query_parser.py
@@ -0,0 +1,167 @@
+# def stringify
+
+from radis.search.utils.query_parser import (
+    BinaryNode,
+    Node,
+    ParensNode,
+    QueryParser,
+    TermNode,
+    UnaryNode,
+)
+
+
+def unparse(node: Node):
+    if isinstance(node, str):
+        return node
+    elif isinstance(node, UnaryNode):
+        return f"{node.operator} {unparse(node.operand)}"
+    elif isinstance(node, BinaryNode):
+        if node.implicit:
+            return f"{unparse(node.left)} {unparse(node.right)}"
+        return f"{unparse(node.left)} {node.operator} {unparse(node.right)}"
+    elif isinstance(node, ParensNode):
+        return f"({unparse(node.expression)})"
+    elif isinstance(node, TermNode):
+        if node.term_type == "WORD":
+            return node.value
+        elif node.term_type == "PHRASE":
+            return f'"{node.value.replace('"', '\\"')}"'
+    else:
+        raise ValueError(f"Unknown node type: {type(node)}")
+
+
+def is_valid_query(query: str, final_query: str | None = None) -> bool:
+    if final_query is None:
+        final_query = query
+    node, fixes = QueryParser().parse(query)
+    assert len(fixes) == 0
+    assert node is not None
+    assert unparse(node) == final_query
+    return True
+
+
+def is_fixed_query(query: str, fixed_query: str, num_of_fixes: int) -> bool:
+    node, fixes = QueryParser().parse(query)
+    assert len(fixes) == num_of_fixes
+    assert node is not None
+    assert unparse(node) == fixed_query
+    return True
+
+
+def is_empty_query(query: str, num_of_fixes: int) -> bool:
+    node, fixes = QueryParser().parse(query)
+    assert len(fixes) == num_of_fixes
+    assert node is None
+    return True
+
+
+def test_parse_valid_queries():
+    # Words
+    assert is_valid_query("foobar")
+    assert is_valid_query("Hämatom")
+    assert is_valid_query("Magen-Darm-Trakt")
+
+    # Phrases
+    assert is_valid_query('"foo bar"')
+    assert is_valid_query('"foo ( bar"')
+    assert is_valid_query('"foo \\" bar"')
+
+    # Implicit AND operator
+    assert is_valid_query("foo bar")
+
+    # Explicit AND operator
+    assert is_valid_query("foo AND bar")
+
+    # OR operator
+    assert is_valid_query("foo OR bar")
+
+    # NOT operator
+    assert is_valid_query("NOT foo")
+
+    # Parentheses
+    assert is_valid_query("(foo)")
+
+    # More complex queries
+    assert is_valid_query("foo AND bar OR baz")
+    assert is_valid_query("foo AND (bar OR baz)")
+    assert is_valid_query("foo AND (bar OR NOT baz)")
+    assert is_valid_query('foo AND (bar OR NOT "baz qux") AND qux')
+    assert is_valid_query('foo AND (NOT "bar baz" (moo OR zoo) AND yoo)')
+    assert is_valid_query('foo AND (NOT "bar ( baz" (moo OR zoo) AND yoo)')
+
+
+def test_shortened_queries():
+    # Whitespace at start of line
+    assert is_valid_query("   foo bar", "foo bar")
+
+    # Whitespace at end of line
+    assert is_valid_query("foo bar   ", "foo bar")
+
+    # Redundant whitespace in between
+    assert is_valid_query("foo   bar", "foo bar")
+
+    # Whitespace after opening parenthesis
+    assert is_valid_query("(   foo bar)", "(foo bar)")
+
+    # Whitespace before closing parenthesis
+    assert is_valid_query("(foo bar   )", "(foo bar)")
+
+    # Don't fix whitespace in quoted terms
+    assert is_valid_query('"foo   bar"')
+
+
+def test_fixed_queries():
+    # Fix unary operator before binary operator
+    assert is_fixed_query("foo NOT AND bar", "foo NOT bar", 1)
+
+    # Fix invalid consecutive operators
+    assert is_fixed_query("foo AND AND bar", "foo AND bar", 1)
+    assert is_fixed_query("foo AND OR bar", "foo AND bar", 1)
+    assert is_fixed_query("foo AND OR AND bar", "foo AND bar", 1)
+    assert is_fixed_query("foo AND OR NOT bar", "foo AND NOT bar", 1)
+    assert is_fixed_query("foo AND NOT OR NOT bar", "foo AND NOT bar", 1)
+
+    # Fix invalid binary operator at start of line
+    assert is_fixed_query("AND foo bar", "foo bar", 1)
+    assert is_fixed_query("OR foo bar", "foo bar", 1)
+
+    # Fix invalid operator at end of line
+    assert is_fixed_query("foo bar NOT", "foo bar", 1)
+    assert is_fixed_query("foo bar AND", "foo bar", 1)
+    assert is_fixed_query("foo bar OR", "foo bar", 1)
+
+    # Fix invalid binary operator at start of parentheses expression
+    assert is_fixed_query("(AND foo bar)", "(foo bar)", 1)
+    assert is_fixed_query("(OR foo bar)", "(foo bar)", 1)
+
+    # Fix invalid operator at end of parentheses expression
+    assert is_fixed_query("(foo bar NOT)", "(foo bar)", 1)
+    assert is_fixed_query("(foo bar AND)", "(foo bar)", 1)
+    assert is_fixed_query("(foo bar OR)", "(foo bar)", 1)
+
+    # Fix unbalanced parentheses
+    assert is_fixed_query("(foo bar", "foo bar", 1)
+    assert is_fixed_query("foo bar)", "foo bar", 1)
+
+    # Fix unbalanced quotes
+    assert is_fixed_query('"foo bar', "foo bar", 1)
+    assert is_fixed_query('foo bar"', "foo bar", 1)
+    assert is_fixed_query('"foo bar" baz"', '"foo bar" baz', 1)
+
+    # Fix unvalid characters
+    assert is_fixed_query("foo$bar", "foobar", 1)
+    assert is_fixed_query("foo bar~ baz", "foo bar baz", 1)
+    assert is_fixed_query('foo bar \\" baz', "foo bar baz", 1)
+
+    # Multiple fixes
+    assert is_fixed_query("foo \\) bar", "foo bar", 2)
+
+
+def test_empty_queries():
+    assert is_empty_query("", 0)
+    assert is_empty_query("   ", 0)
+    assert is_empty_query("()", 1)
+    assert is_empty_query("(   )", 1)
+    assert is_empty_query("( AND )", 2)
+    assert is_empty_query("( AND OR )", 3)
+    assert is_empty_query("AND OR )", 3)
---
+++ radis/search/utils/query_parser.py
@@ -0,0 +1,277 @@
+import re
+from typing import Callable, Literal, cast
+
+import pyparsing as pp
+
+
+class TermNode:
+    def __init__(self, term_type: Literal["WORD", "PHRASE"], value: str):
+        self.term_type = term_type
+        self.value = value
+
+    def __repr__(self):
+        return f"TermNode({self.term_type}, {self.value})"
+
+
+class ParensNode:
+    def __init__(self, expression: "Node"):
+        self.expression = expression
+
+    def __repr__(self):
+        return f"ParensNode({self.expression})"
+
+
+class UnaryNode:
+    def __init__(self, operator: Literal["NOT"], operand: "Node"):
+        self.operator = operator
+        self.operand = operand
+
+    def __repr__(self):
+        return f"UnaryNode({self.operator}, {self.operand})"
+
+
+class BinaryNode:
+    def __init__(
+        self,
+        operator: Literal["AND", "OR"],
+        left: "Node",
+        right: "Node",
+        implicit: bool = False,
+    ):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+        if implicit and operator != "AND":
+            raise ValueError("Implicit operator can only be an AND")
+
+        self.implicit = implicit
+
+    def __repr__(self):
+        return f"BinaryNode({self.operator}, {self.left}, {self.right})"
+
+
+Node = TermNode | UnaryNode | BinaryNode | str
+
+
+class QueryParser:
+    def __init__(self):
+        pass
+
+    def _replace_unbalanced_quotes(self, input_string: str) -> str:
+        result = []
+        in_quote = False  # Track if we are inside quotes
+        last_unescaped_quote_index = -1
+
+        i = 0
+        while i < len(input_string):
+            if input_string[i] == '"' and (
+                i == 0 or input_string[i - 1] != "\\"
+            ):  # Check for unescaped quote
+                in_quote = not in_quote
+                result.append(input_string[i])
+                if in_quote:
+                    last_unescaped_quote_index = i
+            else:
+                result.append(input_string[i])
+            i += 1
+
+        # If we are still inside a quote after processing, it means there is an unbalanced quote
+        if in_quote and last_unescaped_quote_index != -1:
+            result[last_unescaped_quote_index] = " "
+
+        return "".join(result)
+
+    def _replace_unbalanced_parentheses(self, input_string: str) -> str:
+        stack = []
+        result = list(input_string)
+        inside_quotes = False
+        escape_next = False
+
+        for i, char in enumerate(input_string):
+            if escape_next:
+                escape_next = False
+            elif char == "\\":
+                escape_next = True
+            elif char == '"' and not escape_next:
+                inside_quotes = not inside_quotes
+            if char == "(" and not inside_quotes:
+                stack.append(i)
+            elif char == ")" and not inside_quotes:
+                if stack:
+                    stack.pop()
+                else:
+                    result[i] = " "
+
+        while stack:
+            result[stack.pop()] = " "
+
+        return "".join(result)
+
+    def _modify_unquoted_segments(
+        self, input_string: str, unquoted_segment_handler: Callable[[str], str]
+    ) -> str:
+        pattern = re.compile(r'(".*?(?<!\\)")')
+        parts = pattern.split(input_string)
+
+        results: list[str] = []
+        for part in parts:
+            if part.startswith('"'):
+                assert part.endswith('"')
+                results.append(part)
+            else:
+                results.append(unquoted_segment_handler(part))
+
+        return "".join(results)
+
+    def _replace_invalid_characters(self, input_string: str) -> str:
+        valid_chars = pp.alphanums + pp.alphas8bit + "_-'() "
+
+        def handle_segment(segment: str) -> str:
+            return "".join(char for char in segment if char in valid_chars)
+
+        return self._modify_unquoted_segments(input_string, handle_segment)
+
+    def _clean_consecutive_operators(self, input_string) -> str:
+        pattern = r"((?:NOT|AND|OR)\b\s*)(?:(?:NOT|AND|OR)\b\s*)*(?:(?:AND|OR)\b\s*)+((?:NOT\b)*)"
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(pattern, r"\1\2", s))
+
+    def _clean_binary_operators_at_start_of_line(self, input_string: str) -> str:
+        pattern = r"^(\s*(AND|OR))+\s*"
+        cleaned_string = re.sub(pattern, r"", input_string)
+        return cleaned_string
+
+    def _clean_operators_at_end_of_line(self, input_string: str) -> str:
+        pattern = r"(\s*(NOT|AND|OR))+\s*$"
+        cleaned_string = re.sub(pattern, r"", input_string)
+        return cleaned_string
+
+    def _clean_binary_operators_at_start_of_parens(self, input_string: str) -> str:
+        pattern = r"\((\s*(AND|OR))+\s*"
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(pattern, r"(", s))
+
+    def _clean_operators_at_end_of_parens(self, input_string: str) -> str:
+        pattern = r"(\s*(NOT|AND|OR))+\s*\)"
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(pattern, r")", s))
+
+    def _clean_empty_parens(self, input_string: str) -> str:
+        pattern = r"\(\s*\)"
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(pattern, r"", s))
+
+    def _strip_trailing_spaces(self, input_string: str) -> str:
+        cleaned_string = input_string.strip()
+        return cleaned_string
+
+    def _delete_spaces_at_start_of_parens(self, input_string: str):
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(r"\(\s*", "(", s))
+
+    def _delete_spaces_at_end_of_parens(self, input_string: str) -> str:
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(r"\s*\)", ")", s))
+
+    def _reduce_spaces(self, input_string: str) -> str:
+        return self._modify_unquoted_segments(input_string, lambda s: re.sub(r"\s+", " ", s))
+
+    def _parse_string(self, input_string: str) -> Node:
+        not_ = pp.Keyword("NOT")
+        and_ = pp.Keyword("AND")
+        or_ = pp.Keyword("OR")
+        lparen = pp.Literal("(")
+        rparen = pp.Literal(")")
+
+        word = ~(not_ | and_ | or_) + pp.Word(
+            pp.alphanums + pp.alphas8bit + "_-'"
+        ).set_parse_action(
+            lambda t: TermNode("WORD", t[0])  # type: ignore
+        )
+        phrase = pp.QuotedString(quoteChar='"', esc_char="\\").set_parse_action(
+            lambda t: TermNode("PHRASE", t[0])  # type: ignore
+        )
+        term = phrase | word
+
+        or_expression = pp.Forward()
+
+        parens_expression = pp.Forward()
+        parens_expression <<= (
+            pp.Suppress(lparen) + or_expression + pp.Suppress(rparen)
+        ).set_parse_action(lambda t: ParensNode(t[0])) | term  # type: ignore
+
+        not_expression = pp.Forward()
+        not_expression <<= (not_ + not_expression).set_parse_action(
+            lambda t: UnaryNode("NOT", t[1])  # type: ignore
+        ) | parens_expression
+
+        and_expression = pp.Forward()
+        and_expression <<= (
+            (not_expression + and_ + and_expression).set_parse_action(
+                lambda t: BinaryNode("AND", t[0], t[2])  # type: ignore
+            )
+            | (not_expression + and_expression).set_parse_action(
+                lambda t: BinaryNode("AND", t[0], t[1], implicit=True)  # type: ignore
+            )
+            | not_expression
+        )
+
+        or_expression <<= (and_expression + or_ + or_expression).set_parse_action(
+            lambda t: BinaryNode("OR", t[0], t[2])  # type: ignore
+        ) | and_expression
+
+        return cast(Node, or_expression.parse_string(input_string, parse_all=True)[0])
+
+    def parse(self, query: str) -> tuple[Node | None, list[str]]:
+        fixes: list[str] = []
+
+        query_before = query
+        query_after = self._replace_unbalanced_quotes(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed unbalanced quotes")
+
+        query_before = query_after
+        query_after = self._replace_unbalanced_parentheses(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed unbalanced parentheses")
+
+        query_before = query_after
+        query_after = self._replace_invalid_characters(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid characters")
+
+        query_before = query_after
+        query_after = self._clean_consecutive_operators(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid consecutive operators")
+
+        query_before = query_after
+        query_after = self._clean_binary_operators_at_start_of_line(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid operators at start of line")
+
+        query_before = query_after
+        query_after = self._clean_operators_at_end_of_line(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid operators at end of line")
+
+        query_before = query_after
+        query_after = self._clean_binary_operators_at_start_of_parens(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid operators at start of parentheses")
+
+        query_before = query_after
+        query_after = self._clean_operators_at_end_of_parens(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed invalid operators at end of parentheses")
+
+        query_before = query_after
+        query_after = self._clean_empty_parens(query_before)
+        if query_before != query_after:
+            fixes.append("Fixed empty parentheses")
+
+        query_after = self._strip_trailing_spaces(query_after)
+        query_after = self._delete_spaces_at_start_of_parens(query_after)
+        query_after = self._delete_spaces_at_end_of_parens(query_after)
+        query_after = self._reduce_spaces(query_after)
+
+        if query_after == "":
+            return None, fixes
+
+        node = self._parse_string(query_after)
+        return node, fixes
