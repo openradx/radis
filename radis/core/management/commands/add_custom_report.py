702c2c435a23c259cadd299caf18b4ad7ed3ab22
Kai Schlamp <kai.schlamp@gmail.com>
2024-11-05 01:09:34 +0000

    Refactor report commands

--- radis/core/management/commands/add_custom_report.py
+++ radis/core/management/commands/add_custom_report.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand, CommandParser
 
-from .populate_example_reports import create_report
+from radis.reports.factories import LanguageFactory, ReportFactory
 
 
 class Command(BaseCommand):
@@ -10,21 +10,40 @@ class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
         super().add_arguments(parser)
 
+        parser.add_argument("--body", type=str, help="The body of the report.")
         parser.add_argument(
-            "--report-body",
-            type=str,
-            help="The body of the report.",
+            "--lng",
+            default="en",
+            help="The language of the report ('en' or 'de'). Defaults to 'en'.",
         )
         parser.add_argument(
-            "--report-language",
-            default="en",
-            help="The language of the report (en or de).",
+            "--group",
+            type=int,
+            default=None,
+            help="The group to assign the report to. "
+            "If not set then the first group in the database will be used.",
         )
 
     def handle(self, *args, **options):
-        group = Group.objects.first()
+        if options["group"] is not None:
+            try:
+                group = Group.objects.get(id=options["group"])
+            except Group.DoesNotExist:
+                raise ValueError(f"Group with ID {options['group']} does not exist.")
+        else:
+            group = Group.objects.order_by("id").first()
+            if group is None:
+                raise ValueError("No group found. Please create a group first.")
 
-        if not group:
-            raise ValueError("No group found. Please create a group.")
+        body = options["body"]
+        language = options["lng"]
 
-        create_report(options["report_body"], options["report_language"], group)
+        self.stdout.write(
+            f"Adding custom report to group '{group.name}' into the database...", ending=""
+        )
+        self.stdout.flush()
+
+        report = ReportFactory.create(language=LanguageFactory(code=language), body=body)
+        report.groups.set([group])
+
+        self.stdout.write("Done")
--- radis/core/management/commands/populate_example_reports.py
+++ radis/core/management/commands/populate_example_reports.py
@@ -1,7 +1,6 @@
 import json
 import time
 from pathlib import Path
-from typing import Literal
 
 from django.conf import settings
 from django.contrib.auth.models import Group
@@ -9,34 +8,8 @@ from django.core.management.base import BaseCommand, CommandParser
 from faker import Faker
 
 from radis.reports.factories import LanguageFactory, ReportFactory
-from radis.reports.models import Report
 
 fake = Faker()
-
-
-def create_report(body: str, language: Literal["en", "de"], group: Group):
-    report = ReportFactory.create(language=LanguageFactory(code=language), body=body)
-    report.groups.set([group])
-    return report
-
-
-def create_reports(language: Literal["en", "de"], group: Group):
-    if language == "en":
-        sample_file = "reports_en.json"
-    elif language == "de":
-        sample_file = "reports_de.json"
-    else:
-        raise ValueError(f"Language {language} is not supported.")
-
-    samples_path = Path(settings.BASE_DIR / "samples" / sample_file)
-    with open(samples_path, "r") as f:
-        report_bodies = json.load(f)
-
-    start = time.time()
-    reports: list[Report] = []
-    for report_body in report_bodies:
-        reports.append(create_report(report_body, language, group))
-    print(f"Generated {len(reports)} reports in {time.time() - start:.2f} seconds.")
 
 
 class Command(BaseCommand):
@@ -46,25 +19,51 @@ class Command(BaseCommand):
         super().add_arguments(parser)
 
         parser.add_argument(
-            "--skip-reports",
-            action="store_true",
-            help="Skip populating the database with example reports.",
+            "--group",
+            type=int,
+            default=None,
+            help="The group to assign the reports to. "
+            "If not set then the first group in the database will be used.",
         )
         parser.add_argument(
-            "--report-language",
+            "--lng",
             default="en",
-            help="Which report language to use (en or de).",
+            help="Which report language to use ('en' or 'de'). Defaults to 'en'.",
         )
 
     def handle(self, *args, **options):
-        group = Group.objects.first()
+        if options["group"] is not None:
+            try:
+                group = Group.objects.get(id=options["group"])
+            except Group.DoesNotExist:
+                raise ValueError(f"Group with ID {options['group']} does not exist.")
+        else:
+            group = Group.objects.order_by("id").first()
+            if group is None:
+                raise ValueError("No group found. Please create a group first.")
 
-        if not group:
-            raise ValueError("No group found. Please create a group.")
+        language = options["lng"]
+        if language == "en":
+            sample_file = "reports_en.json"
+        elif language == "de":
+            sample_file = "reports_de.json"
+        else:
+            raise ValueError(f"Language {language} is not supported.")
 
-        if not options["skip_reports"]:
-            if Report.objects.first():
-                print("Reports already populated. Skipping.")
-            else:
-                print("Populating database with example reports.")
-                create_reports(options["report_language"], group)
+        samples_path = Path(settings.BASE_DIR / "samples" / sample_file)
+        with open(samples_path, "r") as f:
+            report_bodies = json.load(f)
+
+        self.stdout.write(
+            f"Adding {len(report_bodies)} example reports "
+            f"to group '{group.name}' into the database...",
+            ending="",
+        )
+        self.stdout.flush()
+
+        start = time.time()
+        for report_body in report_bodies:
+            report = ReportFactory.create(language=LanguageFactory(code=language), body=report_body)
+            report.groups.set([group])
+
+        self.stdout.write(f"Done (in {time.time() - start:.2f} seconds)")
