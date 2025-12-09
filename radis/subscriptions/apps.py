1c8db1cda90148ae65e724f156735f1423e2605c
Samuel Kwong <samvictor232@gmail.com>
2025-12-09 23:28:41 +0000

    move necessary imports inside register reports handler

--- radis/subscriptions/apps.py
+++ radis/subscriptions/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-from radis.reports.site import ReportsCreatedHandler, reports_created_handlers
-
-from .handlers import handle_reports_created
-
 
 class SubscriptionsConfig(AppConfig):
     name = "radis.subscriptions"
@@ -37,6 +33,9 @@ def init_db(**kwargs):
 
 def register_reports_handler():
     """Register handler to trigger subscriptions when reports are created."""
+    from radis.reports.site import ReportsCreatedHandler, reports_created_handlers
+
+    from .handlers import handle_reports_created
 
     reports_created_handlers.append(
         ReportsCreatedHandler(
