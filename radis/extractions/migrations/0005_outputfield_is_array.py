9235cd93a3cb7c2be8227d501a5a99452962b8d6
Samuel Kwong <samvictor232@gmail.com>
2025-12-14 23:50:24 +0000

    fix migration order and duplicate migration

--- radis/extractions/migrations/0005_outputfield_is_array.py
+++ radis/extractions/migrations/0005_outputfield_is_array.py
@@ -7,22 +7,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # Track whether an output field should return a list of values rather than a single value.
-        migrations.AddField(
-            model_name="outputfield",
-            name="is_array",
-            field=models.BooleanField(default=False),
-        ),
-    ]
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("extractions", "0004_outputfield_selection_options"),
-    ]
-
-    operations = [
         migrations.AddField(
             model_name="outputfield",
             name="is_array",
